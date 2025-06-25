86d6484519a17704006017bd33b81aea1b67d015
juanfelenis <lenisserna2001@gmail.com>
2025-06-25 21:43:37 +0200

    -Archivo /.htaccess mejorado y fallback añadido -Sitemap.xml mejorado y todas las rutas añadidas

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   base: '/',
@@ -8,10 +8,16 @@ export default defineConfig({
     react(),
     Sitemap({
       hostname: 'https://leonti.eu',
-      outDir: 'docs'
+      outDir: 'docs',
+      routes: [
+        '/',
+        '/services',
+        '/contact',
+        '/#prices',
+        '/#gallery',
+        '/#cnc-shop'
+      ]
     })
   ],
-  build: {
-    outDir: 'docs',
-  },
-})
+  build: { outDir: 'docs' }
+});
