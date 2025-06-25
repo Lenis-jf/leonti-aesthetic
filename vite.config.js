cd5b7bd527ba8d8a8d45fab1b3a303f7e07bedd5
juanfelenis <lenisserna2001@gmail.com>
2025-06-25 22:56:43 +0200

    haciendo cambios manuales en el sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   base: '/',
   plugins: [
-    react(),
-    Sitemap({
-      hostname: 'https://leonti.eu',
-      outDir: 'docs',
-      routes: [
-        '/',
-        '/services',
-        '/contact',
-        '/#prices',
-        '/#gallery',
-        '/#cnc-shop'
-      ]
-    })
+    react()
   ],
-  build: { outDir: 'docs' }
+  build: {
+    outDir: 'docs'
+  }
 });
